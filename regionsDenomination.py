7c7c4500a331adf67bdb4eb0f0abbf8f79103294
Riccardo Maldini <riccardo.maldini@gmail.com>
2020-11-19 13:47:21 +0100

    Add missing dectionary files

---
+++ regionsDenomination.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Correct name of each region, as stated in the field 'denominazione_regione' of the df
+@author: riccardomaldini
+"""
+
+abruzzo = "Abruzzo"
+basilicata = "Basilicata"
+calabria = "Calabria"
+campania = "Campania"
+emilia_romagna = "Emilia-Romagna"
+friuli = "Friuli Venezia Giulia"
+lazio = "Lazio"
+liguria = "Liguria"
+lombardia = "Lombardia"
+marche = "Marche"
+molise = "Molise"
+bolzano = "P.A. Bolzano"
+trento = "P.A. Trento"
+piemonte = "Piemonte"
+puglia = "Puglia"
+sardegna = "Sardegna"
+sicilia = "Sicilia"
+toscana = "Toscana"
+umbria = "Umbria"
+valle_daosta = "Valle d'Aosta"
+veneto = "Veneto"
