cca59820fa0847bec6f25d6a02995c9038128f8e
Riccardo Maldini <riccardo.maldini@gmail.com>
2021-04-13 16:44:11 +0200

    Rewrite table without bootstrap

--- client/src/app/shared/components/summary-table/summary-table.component.ts
+++ client/src/app/shared/components/summary-table/summary-table.component.ts
@@ -1,9 +1,10 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-summary-table',
   templateUrl: './summary-table.component.html',
-  styleUrls: ['./summary-table.component.scss']
+  styleUrls: ['./summary-table.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class SummaryTableComponent implements OnInit {
   constructor() { }
